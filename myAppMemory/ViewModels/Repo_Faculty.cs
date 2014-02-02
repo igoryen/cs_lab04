70cb7a5363523ebdf2d3703d2330a5f1d4a6f2cb
igoryen <igor.yentaltsev@gmail.com>
2014-02-02 18:46:28 -0500

    ViewModels/Repo_Faculty.cs/getFacultiesPublic(): added

--- myAppMemory/ViewModels/Repo_Faculty.cs
+++ myAppMemory/ViewModels/Repo_Faculty.cs
@@ -32,6 +32,22 @@ namespace myAppMemory.ViewModels {
 
 
     // return a list / collection of faculties
+    public IEnumerable<FacultyPublic> getFacultiesPublic() { // 1
+      var ls = Faculties.OrderBy(n => n.FacultyId); // 20
+      List<FacultyPublic> rls = new List<FacultyPublic>(); // 25
+
+      foreach (var item in ls) {  // 30
+        FacultyPublic row = new FacultyPublic();   // 35
+
+        row.FacultyId = item.FacultyId;  // 40 
+        row.FirstName = item.FirstName;
+        row.LastName = item.LastName;
+        row.Phone = item.Phone;
+
+        rls.Add(row);  // 45 
+      }
+      return rls;  // 50
+    }
 
 
     public List<Faculty> Faculties { get; set; }
