f4698330835d5ad0f068f3f07fd0c8424f3fe01b
igoryen <igor.yentaltsev@gmail.com>
2014-02-02 18:13:13 -0500

    ViewModels/Repo_Faculty.cs/Repo_Faculty(): added

--- myAppMemory/ViewModels/Repo_Faculty.cs
+++ myAppMemory/ViewModels/Repo_Faculty.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using myAppMemory.Models;
 
 namespace myAppMemory.ViewModels {
   public class Repo_Faculty {
+
+    public Repo_Faculty() {
+      this.Faculties = (List<Faculty>)HttpContext.Current.Application["Faculties"];
+    }
+
+    // create a Faculty record
+
+
+    // return single faculty row by id
+
+
+    // return a list / collection of faculties
   }
 }
