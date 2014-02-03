f2dd53dddbbfcab539a734dd2288c38c2ea44a7d
igoryen <igor.yentaltsev@gmail.com>
2014-02-03 18:38:35 -0500

    Models/DataContext.cs: filled

--- myAppMemory/Models/DataContext.cs
+++ myAppMemory/Models/DataContext.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace myAppMemory.Models {
-  public class DataContext {
+  public class DataContext : DbContext {
+    public DataContext() : base("name=DataContext") { }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Faculty> Faculty { get; set; }
+    public DbSet<Course> Courses { get; set; }
+
+    public System.Data.Entity.DbSet<myAppMemory.ViewModels.StudentFull> StudentFulls { get; set; }
   }
 }
