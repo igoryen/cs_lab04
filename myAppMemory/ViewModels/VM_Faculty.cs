f307acd93d11ed872504a774931b8b631ab913b5
igoryen <igor.yentaltsev@gmail.com>
2014-02-04 16:34:29 -0500

    ViewModels/VM_Faculty.cs/'using myAppMemory.Models': added

--- myAppMemory/ViewModels/VM_Faculty.cs
+++ myAppMemory/ViewModels/VM_Faculty.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using myAppMemory.Models;
 
 namespace myAppMemory.ViewModels {
   public class VM_Faculty {
