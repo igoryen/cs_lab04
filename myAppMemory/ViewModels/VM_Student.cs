5aeb8d184de824515db238470d05dde11977003c
igoryen <igor.yentaltsev@gmail.com>
2014-02-03 19:32:36 -0500

    ViewModels/VM_Student.cs: changed

--- myAppMemory/ViewModels/VM_Student.cs
+++ myAppMemory/ViewModels/VM_Student.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using myAppMemory.Models;
 
 namespace myAppMemory.ViewModels {
   public class StudentsForList {
@@ -15,6 +16,9 @@ namespace myAppMemory.ViewModels {
   }
 
   public class StudentFull : StudentsForList {
+    public StudentFull() {
+      this.Courses = new List<CourseForList>();
+    }
     [Required]
     [StringLength(40, MinimumLength = 3)]
     [Display(Name = "First Name")]
@@ -27,9 +31,13 @@ namespace myAppMemory.ViewModels {
     [Required]
     [RegularExpression("^[2-9]\\d{2}-\\d{3}-\\d{4}$", ErrorMessage = "nnn-nnn-nnnn")]
     public string Phone { get; set; }
+    public List<CourseForList> Courses { get; set; }
   }
 
   public class StudentPublic {
+    
+    [Key]
+    public int StudentId { get; set; }
 
     [Required]
     [RegularExpression("^[0][0-9]{8}$", ErrorMessage = "0 followed by 8 digits")]
