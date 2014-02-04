92f6b54b276c68a1bff5835ed1d5d185be899c25
igoryen <igor.yentaltsev@gmail.com>
2014-02-03 19:39:23 -0500

    ViewModels/Repo_Courses.cs: filled out

--- myAppMemory/ViewModels/Repo_Courses.cs
+++ myAppMemory/ViewModels/Repo_Courses.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using System.Web;
 
 namespace myAppMemory.ViewModels {
-  public class Repo_Courses {
+  public class Repo_Courses : RepositoryBase {
+
+    public static List<CourseForList> getCourseForList(List<myAppMemory.Models.Course> ls) {
+      List<CourseForList> nls = new List<CourseForList>();
+
+      foreach (var item in ls) {
+        CourseForList co = new CourseForList();
+        co.CourseCode = item.CourseCode;
+        co.CourseId = item.Id;
+        nls.Add(co);
+      }
+
+      return nls;
+    }
   }
 }
