fe2efd25bf6c0078db035a5dd27c754b44119938
igoryen <igor.yentaltsev@gmail.com>
2014-02-03 19:49:37 -0500

    ViewModels/RepositoryBase.cs: filled out

--- myAppMemory/ViewModels/RepositoryBase.cs
+++ myAppMemory/ViewModels/RepositoryBase.cs
@@ -2,8 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using myAppMemory.ViewModels;
 
 namespace myAppMemory.ViewModels {
+
   public class RepositoryBase {
+
+    protected DataContext dc;
+
+    public RepositoryBase() {
+
+      dc = new DataContext();
+
+      dc.Configuration.ProxyCreationEnabled = false; // 20
+
+      dc.Configuration.LazyLoadingEnabled = false; // 25
+    }
   }
 }
+
+/*
+ * 20. Turn off the Entity Framework (EF) proxy creation features
+       We do NOT want the EF to track changes - we'll do that ourselves
+ * 25. Also, turn off lazy loading...
+       We want to retain control over fetching related objects
+ */
